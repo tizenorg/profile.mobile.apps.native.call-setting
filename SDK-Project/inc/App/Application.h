8e2eca5c20f88ceeef7b5ad37190921ed8bf3bd7
Andriy Kolesnichenko <a.kolesniche@samsung.com>
2016-02-16 20:10:44 +0200

    [TizenRefApp-5741][Call Setting] Implement Application class
    Change-Id: I0cbc1aae8715d4e5e09d6e01609a31e1e12e9512
    Signed-off-by: Andriy Kolesnichenko <a.kolesniche@samsung.com>

---
+++ SDK-Project/inc/App/Application.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+#include <app.h>
+#include "App/AppCore.h"
+
+namespace App {
+	class Application
+	{
+	public:
+		Application();
+		~Application();
+		int appRun(int argc, char *argv[]);
+		void appTerminate();
+
+	private:
+		AppCore *m_pAppCore;
+		bool onAppCreate();
+		void onAppTerminate();
+		void onAppPause();
+		void onAppResume();
+		void onAppControl(app_control_h request);
+		void onLanguageChanged(app_event_info_h event);
+	};
+}
+
+#endif /* APPLICATION_H_ */
