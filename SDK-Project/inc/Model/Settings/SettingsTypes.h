b3d6b9d449194853f273282862d7ad4a266fac8b
Andriy Kolesnichenko <a.kolesniche@samsung.com>
2016-02-17 19:21:37 +0200

    TizenRefApp-5742 [Call Setting] Prepare sceleton for Model classes
    Change-Id: I8758c8f86265384b00ef2a4aa5f16c41a5c5d0b6
    Signed-off-by: Andriy Kolesnichenko <a.kolesniche@samsung.com>

---
+++ SDK-Project/inc/Model/Settings/SettingsTypes.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef SETTINGS_TYPES_H_
+#define SETTINGS_TYPES_H_
+
+namespace Model { namespace Settings {
+	typedef enum {
+		SETTINGS_RES_SUCCESS,
+		SETTINGS_RES_FAIL_KEY_UNKNOWN,
+		SETTINGS_RES_FAIL_KEY_TYPE_WRONG,
+		SETTINGS_RES_FAIL_DATA_MISSING,
+		SETTINGS_RES_FAIL_UNEXPECTED_DATA,
+		SETTINGS_RES_FAIL_UNKNOWN_ERROR,
+	} ResultCode;
+
+	typedef enum {
+		BOOL_KEY_CALL_ANSWERING_BY_HOME_KEY,
+		BOOL_KEY_CALL_ENDING_BY_POWER_KEY,
+	} BoolKey;
+
+	typedef enum {
+		INT_KEY_SHOW_CALLER_ID,
+		INT_KEY_REJECT_MSG_COUNT,
+	} IntKey;
+
+	typedef enum {
+		STRING_KEY_REJECT_MSG_ITEM_1,
+		STRING_KEY_REJECT_MSG_ITEM_2,
+		STRING_KEY_REJECT_MSG_ITEM_3,
+		STRING_KEY_REJECT_MSG_ITEM_4,
+		STRING_KEY_REJECT_MSG_ITEM_5,
+		STRING_KEY_REJECT_MSG_ITEM_6,
+	} StringKey;
+} }
+#endif /* SETTINGS_TYPES_H_ */
---
+++ SDK-Project/inc/Model/Telephony/ITelephonyManager.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef I_TELEPHONY_MANAGER_H_
+#define I_TELEPHONY_MANAGER_H_
+
+#include "Model/Telephony/TelephonyTypes.h"
+#include "Model/Telephony/RequestListener.h"
+
+namespace Model { namespace Telephony {
+	class ITelephonyManager {
+	private:
+		ITelephonyManager(ITelephonyManager const &) = delete;
+		ITelephonyManager& operator = (ITelephonyManager const &) = delete;
+
+	public:
+		ITelephonyManager();
+		virtual ~ITelephonyManager();
+		virtual ResultCode requestCallWaitState(CallWaitingReqData &reqData, RequestListener<CallWaitingReqData> &listener) = 0;
+		virtual ResultCode requestCallWaitSetup(CallWaitingReqData &reqData, SimpleRequestListener &listener) = 0;
+		virtual ResultCode requestCallFwdState(CallFwdReqData &reqData, RequestListener<CallFwdReqData> &listener) = 0;
+		virtual ResultCode requestCallFwdSetup(CallFwdReqData &reqData, SimpleRequestListener &listener) = 0;
+		virtual void cancelRequest(int requestId) = 0;
+		virtual void cancellAllRequests() = 0;
+		virtual SimCardState getSimState() = 0;
+		virtual int getSimSlotCount() = 0;
+		virtual ResultCode setActiveSlot(SimSlot slotId) = 0;
+		virtual SimSlot getActiveSlot() = 0;
+	};
+} }
+
+#endif /* I_TELEPHONY_MANAGER_H_ */
