ff2766d7f38cfcbf07ad4f437d6df122cd88edc1
Andriy Kolesnichenko <a.kolesniche@samsung.com>
2016-03-01 21:54:49 +0200

    TizenRefApp-5785 [Call Setting] Prepare sceleton for WidgetBase class
    Change-Id: If69fa0a7386a7f1b3d63ff2700aaacbd1f0cfc93
    Signed-off-by: Andriy Kolesnichenko <a.kolesniche@samsung.com>

---
+++ SDK-Project/inc/View/Widgets/Widget.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef VIEW_WIDGETS_WIDGET_H_
+#define VIEW_WIDGETS_WIDGET_H_
+
+#include <Elementary.h>
+#include <utility>
+
+#include "Utils/Common.h"
+
+namespace Widgets {
+	class Widget : private NonCopyable {
+
+	public:
+		template <typename WIDGET_TYPE, typename... TYPE_ARGS>
+		static WIDGET_TYPE *create(TYPE_ARGS&&... args);
+
+		static void destroy(Widget *widget);
+		void setDestroyHandler(NotifyHandler handler);
+		Evas_Object *getEvasObject() const;
+
+	protected:
+		Widget();
+		virtual ~Widget();
+		virtual void onEvasObjectDel() {};
+
+	protected:
+		Evas_Object *m_pEvasObject;
+
+	private:
+			static void prepare(Widget *widget);
+			void onEvasObjectDelBase(Evas *e, Evas_Object *obj, void *event_info);
+
+	private:
+		NotifyHandler m_destroyHandler;
+		bool m_isDestroying;
+		bool m_isOwner;
+	};
+
+	class WidgetWrapper : public Widget {
+	public:
+		WidgetWrapper(Evas_Object *object) { m_pEvasObject = object;}
+		virtual ~WidgetWrapper() {}
+	};
+
+	template <typename WIDGET_TYPE, typename... TYPE_ARGS>
+	WIDGET_TYPE *Widget::create(TYPE_ARGS&&... args)
+	{
+		WIDGET_TYPE *instance = new WIDGET_TYPE();
+		if (!instance->initialize(std::forward<TYPE_ARGS>(args)...) || !instance->m_pEvasObject) {
+			Widget::destroy(instance);
+			return nullptr;
+		}
+
+		prepare(instance);
+
+		return instance;
+	}
+}
+
+#endif /* VIEW_WIDGETS_WIDGET_H_ */
