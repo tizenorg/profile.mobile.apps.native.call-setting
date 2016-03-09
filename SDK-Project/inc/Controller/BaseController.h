42a4957066b466b81eadec40cfccb80ffe4098ea
Andriy Kolesnichenko <a.kolesniche@samsung.com>
2016-03-09 12:02:17 +0200

    TizenRefApp-5855 [Call Settings] Prepare Base View Controller interface
    Change-Id: If51587675194b4b90263fa7a1ff609f789b8da4d
    Signed-off-by: Andriy Kolesnichenko <a.kolesniche@samsung.com>

---
+++ SDK-Project/inc/Controller/BaseController.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef BASE_CONTROLLER_H_
+#define BASE_CONTROLLER_H_
+
+namespace Controller {
+	class AppCore;
+	class BaseController :
+		public SystemEventListener,
+		public ViewEventListener
+	{
+	public:
+		BaseController(const AppCore &core);
+		BaseController(const AppCore &core, const BaseController &parent);
+		virtual ~BaseController() {};
+
+		BaseController() = delete;
+		BaseController(const BaseController &obj) = delete;
+		BaseController(BaseController &&obj) = delete;
+		virtual void onPause() override;
+		virtual void onResume() override;
+		virtual void onUpdate(int updateFlag) override;
+		virtual void onViewChangeBegin() override;
+		virtual void onViewChangeEnd() override;
+		bool makeDestroyReqeuest(const BaseController &parent);
+
+	private:
+		const AppCore &core;
+		const BaseController &parent;
+	};
+}
+
+#endif /* BASE_CONTROLLER_H_ */
