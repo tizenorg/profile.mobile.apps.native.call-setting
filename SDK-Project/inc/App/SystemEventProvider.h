5f0351a70c7ccf1cb4d4a269835bfe8183e67975
Andriy Kolesnichenko <a.kolesniche@samsung.com>
2016-03-09 12:02:17 +0200

    TizenRefApp-5855 [Call Settings] Prepare Base View Controller interface
    Change-Id: If51587675194b4b90263fa7a1ff609f789b8da4d
    Signed-off-by: Andriy Kolesnichenko <a.kolesniche@samsung.com>

---
+++ SDK-Project/inc/App/SystemEventProvider.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef SYSTEM_EVENT_PROVIDER_H_
+#define SYSTEM_EVENT_PROVIDER_H_
+
+#include <set>
+#include "App/SystemEventListener.h"
+namespace App {
+	enum {
+		/*System orientation was changed*/
+		CST_APP_ORIENTATION_CHANGED = 1,
+		/* System language was changed */
+		CST_APP_LANGUAGE_CHANGED = 2,
+		/* System region format was changed */
+		CST_APP_REGION_FMT_CHANGED = 4,
+	};
+
+	class SystemEventProvider {
+	public:
+		SystemEventProvider();
+		~SystemEventProvider() {};
+		bool addEventListener(SystemEventListener *listener);
+		void removeEventListener(SystemEventListener *listener);
+		void onLanguageChanged();
+		void onOrientationChanged();
+		void onRegionFormatChanged();
+		void onAppPause();
+		void onAppResume();
+	private:
+		std::set<SystemEventListener*> m_ListenersSet;
+	};
+}
+
+
+
+#endif /* SYSTEM_EVENT_PROVIDER_H_ */
