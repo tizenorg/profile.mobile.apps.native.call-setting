4180750291da6c480950b58104c41398dfc564a3
Andriy Kolesnichenko <a.kolesniche@samsung.com>
2016-03-10 13:53:54 +0200

    TizenRefApp-5786 [Call Setting] Prepare sceleton for ViewManager TizenRefApp-5854 [Call Settings] Prepare sceleton View Manager interface [Implementation] Add ViewManager sceleton and provide base realization
    Change-Id: Ie2498b190c30560425117f2bd0113e854e7d317e
    Signed-off-by: Andriy Kolesnichenko <a.kolesniche@samsung.com>

---
+++ SDK-Project/inc/View/ViewManager/ViewManager.h
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef VIEW_MANAGER_H_
+#define VIEW_MANAGER_H_
+
+#include <Elementary.h>
+#include <list>
+
+#include "View/Widgets/NaviItem.h"
+#include "View/ViewManager/BaseView.h"
+#include "Utils/Common.h"
+
+namespace View {
+	using namespace Widgets;
+
+	class ViewManager {
+	public:
+
+		static ViewManager *create();
+		static void destroy(ViewManager *instance);
+
+		template <class ViewType>
+		static ViewType* pushView(ViewManager &manager, bool showAnimation = true);
+
+		bool popView();
+		bool isTopView(BaseView *view);
+		bool addViewEventHandler(ViewEventHandler handler);
+		void removeViewEventHandler(ViewEventHandler handler);
+
+	private:
+		ViewManager();
+		~ViewManager();
+		bool initialize();
+		bool createWindow();
+		bool createNaviframe();
+		NaviItem *createNaviItem(bool showAnimation);
+		void attachView(BaseView *view,  bool showAnimation);
+		Evas_Object *createSoftwareBackButton();
+		void onHwBackKeyPressed(Evas_Object *obj, void *event_info);
+		void onHwMenuKeyPressed(Evas_Object *obj, void *event_info);
+		void onSoftwareBackKeyPressed(Evas_Object *obj, void *event_info);
+		void onTransitionFinished(Evas_Object *obj, void *event_info);
+		void onOrientationChanged(Evas_Object *obj, void *event_info);
+		void onViewDestroy(BaseView *view);
+		void backButtonClickHandler();
+		void menuButtonClickHandler();
+
+	private:
+		Evas_Object *m_pWindow;
+		Evas_Object *m_pRootLayout;
+		Evas_Object *m_pNaviframe;
+
+		std::list<BaseView *> m_ViewList;
+		Delegation<void(ViewEvent)> m_ViewEventHandlers;
+
+		bool isTransitionState;
+		Ecore_Job * m_pActivateJob;
+	};
+
+	template <class ViewType>
+	ViewType* ViewManager::pushView(ViewManager &manager, bool showAnimation)
+	{
+		NaviItem *naviItem = manager.createNaviItem(showAnimation);
+		RETVM_IF(!naviItem, nullptr, "Failed to create NaviItem!");
+
+		ViewType *view = BaseView::create<ViewType>(naviItem);
+		RETVM_IF(!view, nullptr, "Failed to create view!");
+
+		manager.attachView(view, showAnimation);
+		return view;
+	}
+}
+
+#endif /* VIEW_MANAGER_H_ */
