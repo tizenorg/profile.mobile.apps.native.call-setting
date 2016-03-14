844dabd3d47f775f5a763279b67c142d037d8720
Andriy Kolesnichenko <a.kolesniche@samsung.com>
2016-03-14 22:09:43 +0200

    TizenRefApp-5855 [Call Settings] Prepare Base View Controller interface TizenRefApp-5939 [Call Setting] Implament Base Controller
    Change-Id: Ibaab0d0c4a7317caa2e251062a66b22245516206
    Signed-off-by: Andriy Kolesnichenko <a.kolesniche@samsung.com>

---
+++ SDK-Project/inc/Controller/ViewController.h
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef VIEW_CONTROLLER_H_
+#define VIEW_CONTROLLER_H_
+
+#include "App/AppCore.h"
+#include "Utils/Common.h"
+
+namespace Controller {
+	using namespace App;
+	using namespace View;
+
+	class ViewController;
+	typedef Delegate<void(ViewController *)> DestroyRequestHandler;
+
+	class ViewController : private NonCopyable {
+	public:
+		template <class ViewController>
+		static ViewController *create(App::AppCore &core, DestroyRequestHandler handler);
+		virtual ~ViewController();
+		void makeDestroyReqeuest();
+
+	protected:
+		typedef enum {
+			UPDATE_LANGUAGE = 1,
+			UPDATE_REGION_FORMAT = 2,
+			UPDATE_ORIENTATION = 4,
+		} UpdateFlag;
+
+		ViewController (AppCore &core, DestroyRequestHandler handler);
+		bool initialize();
+		void finalize();
+		virtual bool createView() { return false;}
+		virtual void onShow() {}
+		virtual void onHide() {}
+		virtual void onUpdate(int updateFlag) {}
+		virtual void onActivate() {}
+		virtual void onDeactivate() {}
+		virtual void onMenuKeyPressed() {}
+		virtual void onBackKeyPressed() {}
+
+	protected:
+		App::AppCore &m_Core;
+		DestroyRequestHandler m_destroyRequestHandler;
+		View::BaseView *m_pView;
+
+	private:
+		bool m_isActivated;
+		bool m_isVisible;
+		bool m_isDestroying;
+		int m_updateFlag;
+
+	private:
+		void onSystemEvent(SystemEvent event);
+		void onViewEvent(ViewEvent event);
+
+		void handleViewChangeBegin();
+		void handleViewChangeEnd();
+		void handleMenuKeyPress();
+		void handleBackKeyPress();
+
+		void doPause();
+		void doResume();
+		void doUpdate();
+		void doActivate();
+		void doDeactivate();
+		void doShow();
+		void doHide();
+	};
+
+	template <class CONTROLLER_TYPE>
+	CONTROLLER_TYPE *ViewController::create(AppCore &core, DestroyRequestHandler handler)
+	{
+		CONTROLLER_TYPE *instance = new CONTROLLER_TYPE(core, handler);
+		if (!instance->initialize()) {
+			ERR("Failed to create controller instance!");
+			delete instance;
+			return nullptr;
+		}
+
+		return instance;
+	}
+}
+
+#endif /* VIEW_CONTROLLER_H_ */
