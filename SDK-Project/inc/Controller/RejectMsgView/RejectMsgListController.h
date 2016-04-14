1cf0d05c21521bb4f16098f75f55b45eca2ebf3b
Andriy Kolesnichenko <a.kolesniche@samsung.com>
2016-04-14 21:42:13 +0300

    Implement RejectMessagesListView Controller
    Change-Id: I475543704d0efc2b496e516126b6e29459e90885
    Signed-off-by: Andriy Kolesnichenko <a.kolesniche@samsung.com>

---
+++ SDK-Project/inc/Controller/RejectMsgView/RejectMsgListController.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef _CONTROLLER_REJECT_MSG_LIST_CONTROLLER_H_
+#define _CONTROLLER_REJECT_MSG_LIST_CONTROLLER_H_
+
+#include "gui/Base/ViewController.h"
+
+#include "App/Application.h"
+#include "View/RejectMsgView/RejectMsgListView.h"
+#include "RejectMsgUtils.h"
+
+
+namespace CallSettings { namespace Controller {
+
+	class RejectMsgListController : public gui::ViewController {
+	public:
+		~RejectMsgListController();
+
+	protected:
+		friend class ViewController;
+
+		RejectMsgListController(Application &app, NotiHandler handler);
+		virtual void onBackKeyPressed() override;
+		virtual void onMenuKeyPressed() override;
+		virtual void updateView(int updateFlag) override;
+		bool initialize();
+
+		void onRejectMsgCountChanged();
+		void onRejectMsgTextChanged();
+		void updateRejectMsgList();
+		void updateTitleInfo();
+		void enableSelectMode();
+		void disableSelectMode();
+		void deleteSelectedMsg();
+		void onMsgItemChecked(int msgId, bool isChecked);
+		void onMsgItemClicked(int msgId);
+		void onSelectAllItemChecked(bool isChecked);
+		void onDeleteBtnClick();
+		void onCancelBtnClick();
+		void onMenuOptionDeleteClick();
+		void onMenuOptionCreateClick();
+
+	private:
+		typedef enum {
+			DISPLAY_MODE,
+			SELECT_MODE,
+		} ViewMode;
+
+		Application &m_app;
+		View::RejectMsgListView *m_pMsgListView;
+		ViewMode m_viewMode;
+		int m_msgTotalCount;
+		bool m_needUpdate;
+		bool m_isOptionMenuShown;
+		bool m_isMsgListLocked;
+
+		std::vector<int> m_msgDeleteList;
+	};
+
+} }
+#endif /* _CONTROLLER_REJECT_MSG_LIST_CONTROLLER_H_ */
