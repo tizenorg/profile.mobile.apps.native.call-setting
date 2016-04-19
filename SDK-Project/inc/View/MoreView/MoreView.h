403c2c6b5018ec81d0515dfe8ce4bbd3c99da22a
Andriy Kolesnichenko <a.kolesniche@samsung.com>
2016-04-19 19:15:48 +0300

    Implement More option view
    Change-Id: I57f8e76963a42750226729039577d3a53f6c8849
    Signed-off-by: Andriy Kolesnichenko <a.kolesniche@samsung.com>

---
+++ SDK-Project/inc/View/MoreView/MoreView.h
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef _VIEW_MORE_VIEW_H_
+#define _VIEW_MORE_VIEW_H_
+
+#include "gui/Base/BaseView.h"
+#include "gui/Widgets/Genlist.h"
+#include "gui/Widgets/SimpleListItem.h"
+#include "gui/Widgets/DoubleTextListItem.h"
+
+namespace CallSettings { namespace View {
+
+	class MoreView : public gui::BaseView {
+	public:
+		typedef enum {
+			CALLER_ID_STATUS_DEFAULT,
+			CALLER_ID_STATUS_SHOW,
+			CALLER_ID_STATUS_HIDE,
+		} CallerIdStatus;
+
+		void setCallerIdStatus(CallerIdStatus value);
+		void setCallerIdClickHandler(NotiHandler handler) { m_callerIdClickHandeler = handler;}
+		void setCallForwardClickHandler(NotiHandler handler) { m_callFwdClickHandler = handler;}
+		void setCallWaitingCheckHandler(NotiHandler handler) { m_callWaitingCheckHandler = handler;}
+
+		void setWaitingOptionChecked(bool checked) {m_pCallWaitingOption->setCheckState(checked);}
+		bool isWaitingOptionChecked() { return m_pCallWaitingOption->getCheckState();}
+		void setWaitingOptionPending(bool isPending);
+
+	private:
+		friend class BaseView;
+
+		MoreView(gui::NaviItem *naviItem);
+		virtual ~MoreView();
+		virtual bool createViewContent() override;
+		void onOptionSelected(gui::WidgetItem *item);
+
+	private:
+		gui::Genlist *m_pGenlist;
+		gui::DoubleTextListItem *m_pCallerIdOption;
+		gui::SimpleListItem *m_pCallFwdOption;
+		gui::DoubleTextListItem *m_pCallWaitingOption;
+		NotiHandler m_callerIdClickHandeler;
+		NotiHandler m_callFwdClickHandler;
+		NotiHandler m_callWaitingCheckHandler;
+	};
+
+} }
+#endif /* _VIEW_MORE_VIEW_H_ */
