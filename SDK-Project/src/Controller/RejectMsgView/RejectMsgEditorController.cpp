d6aaf8ebcd5e1afe96b90150fe34959fa408c8df
Andriy Kolesnichenko <a.kolesniche@samsung.com>
2016-04-21 14:56:07 +0300

    Implement RejectMessage Edit View Controller
    Change-Id: I6c1634e8a2e21cc8ca657b35dc7cae44e69b6fc7
    Signed-off-by: Andriy Kolesnichenko <a.kolesniche@samsung.com>

---
+++ SDK-Project/src/Controller/RejectMsgView/RejectMsgEditorController.cpp
@@ -0,0 +1,252 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "Controller/RejectMsgView/RejectMsgEditorController.h"
+#include "View/RejectMsgView/RejectMsgEditView.h"
+
+#include "msg.h"
+
+namespace CallSettings { namespace Controller {
+
+	using namespace Model;
+	using namespace View;
+	using namespace gui;
+
+	RejectMsgEditorController::RejectMsgEditorController(Application &app, NotiHandler handler) :
+		ViewController(app, handler),
+		m_app(app),
+		m_pMsgEditView(nullptr),
+		m_viewMode(VIEW_MODE_CREATE),
+		m_editableMsgId(REJECT_MSG_1),
+		m_smsInputedCharCount(0),
+		m_smsMaxCharCount(0)
+	{
+	}
+
+	RejectMsgEditorController::~RejectMsgEditorController()
+	{
+		m_pMsgEditView->setDoneClickHandler(nullptr);
+		m_pMsgEditView->setCancelClickHandler(nullptr);
+		m_pMsgEditView->setInputFilterHandler(nullptr);
+		m_pMsgEditView->setInputEventHandler(nullptr);
+	}
+
+	bool RejectMsgEditorController::initialize()
+	{
+		RETVM_IF(!initializeView(), false, "Internal error!");
+
+		m_viewMode = VIEW_MODE_CREATE;
+		m_pMsgEditView->setViewTitle("IDS_CST_SK3_CREATE");
+		m_pMsgEditView->setDisabledDoneBtn(true);
+		return true;
+
+	}
+
+	bool RejectMsgEditorController::initialize(RejectMsgId msgKey)
+	{
+		RETVM_IF(!initializeView(), false, "Internal error!");
+
+		std::string msgText;
+		SettingResultCode res = m_app.getSettingsManager().getProperty(convertToStringKey(msgKey), msgText);
+		RETVM_IF(res != SETTINGS_RES_SUCCESS, false, "Failed to get mgs text");
+
+		m_viewMode = VIEW_MODE_EDIT;
+		m_editableMsgId = msgKey;
+		m_pMsgEditView->setMsgText(_(msgText.c_str()));
+		m_pMsgEditView->setViewTitle("IDS_CST_BODY_EDIT");
+		return true;
+	}
+
+	bool  RejectMsgEditorController::initializeView()
+	{
+		RETVM_IF(!ViewController::initialize(), false, "Failed to initialize ViewController!");
+
+		m_pMsgEditView =  gui::ViewManager::pushView<View::RejectMsgEditView>(m_app.getViewManager(), true);
+		RETVM_IF(!m_pMsgEditView, false, "Failed to create view instance!");
+
+		m_pMsgEditView->setDoneClickHandler(
+				NotiHandler::wrap<RejectMsgEditorController, &RejectMsgEditorController::onDoneBtnClick>(this));
+
+		m_pMsgEditView->setCancelClickHandler(
+				NotiHandler::wrap<RejectMsgEditorController, &RejectMsgEditorController::onCancelBtnClick>(this));
+
+		m_pMsgEditView->setInputEventHandler(
+				NotiHandler::wrap<RejectMsgEditorController, &RejectMsgEditorController::onInputEvent>(this));
+
+		m_pMsgEditView->setInputFilterHandler(
+				Editfield::InputFilterHandler::wrap<RejectMsgEditorController, &RejectMsgEditorController::onInputFilterEvent>(this));
+
+		setBaseView(m_pMsgEditView);
+		return true;
+	}
+
+	bool RejectMsgEditorController::updateMsgSettings()
+	{
+		std::string msgText = m_pMsgEditView->getMsgText();
+		SettingResultCode res = SETTINGS_RES_SUCCESS;
+		if (m_viewMode == VIEW_MODE_EDIT) {
+			m_app.getSettingsManager().setProperty(convertToStringKey(m_editableMsgId), msgText);
+			RETVM_IF(res != SETTINGS_RES_SUCCESS, false, "Failed to save message, error: %d", res);
+		} else {
+			int msgCount = 0;
+			res = m_app.getSettingsManager().getProperty(INT_KEY_REJECT_MSG_COUNT, msgCount);
+			RETVM_IF(res != SETTINGS_RES_SUCCESS, false, "Failed to get mgs count");
+
+			for (int i = REJECT_MSG_1 + msgCount; i > REJECT_MSG_1; i--) {
+				std::string oldMsg;
+				RejectMsgId oldMsgId = static_cast<RejectMsgId>(i-1);
+				RejectMsgId newMsgId = static_cast<RejectMsgId>(i);
+				m_app.getSettingsManager().getProperty(convertToStringKey(oldMsgId), oldMsg);
+				m_app.getSettingsManager().setProperty(convertToStringKey(newMsgId), oldMsg);
+			}
+
+			m_app.getSettingsManager().setProperty(convertToStringKey(m_editableMsgId), msgText);
+			msgCount++;
+			m_app.getSettingsManager().setProperty(INT_KEY_REJECT_MSG_COUNT, msgCount);
+
+		}
+		return true;
+
+	}
+
+	bool RejectMsgEditorController::isMsgDuplicated(const std::string &msgText)
+	{
+		int msgCount = 0;
+		m_app.getSettingsManager().getProperty(INT_KEY_REJECT_MSG_COUNT, msgCount);
+
+		for (int i = REJECT_MSG_1; i < REJECT_MSG_1 + msgCount; i++) {
+			std::string existedMsgText;
+			RejectMsgId msgId = static_cast<RejectMsgId>(i);
+			m_app.getSettingsManager().getProperty(convertToStringKey(msgId), existedMsgText);
+			if (msgText.compare(existedMsgText) == 0) {
+				DBG("Message already in use!");
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	void RejectMsgEditorController::onDoneBtnClick()
+	{
+		std::string msgText = m_pMsgEditView->getMsgText();
+		if (isMsgDuplicated(msgText)) {
+			//TODO Add duplicated noti here!
+			DBG("MSG IS DUPLICATED!!!");
+		} else {
+			if (updateMsgSettings()) {
+				makeDestroyReqeuest();
+			}
+		}
+	}
+
+	void RejectMsgEditorController::onCancelBtnClick()
+	{
+		makeDestroyReqeuest();
+	}
+
+	void RejectMsgEditorController::onInputEvent()
+	{
+		std::string smsText = m_pMsgEditView->getMsgText();
+		int inputCount = 0;
+		int maxInputCount = 0;
+
+		RETM_IF(!calculateSMSTextLength(smsText, inputCount, maxInputCount), "Failed to calculate sms length");
+
+		if (inputCount == 0) {
+			m_pMsgEditView->setDisabledDoneBtn(true);
+		} else if ((inputCount != 0 && m_smsInputedCharCount == 0) || (inputCount > maxInputCount)) {
+				m_pMsgEditView->setDisabledDoneBtn(false);
+		}
+
+		m_smsInputedCharCount = inputCount;
+		m_smsMaxCharCount = maxInputCount;
+		m_pMsgEditView->updateInputCharCount(m_smsInputedCharCount, m_smsMaxCharCount);
+	}
+
+	bool RejectMsgEditorController::onInputFilterEvent(std::string &msgText)
+	{
+		int smsCharCount = 0;
+		int smsMaxCharCount = 0;
+
+		std::string entryText = m_pMsgEditView->getMsgText() + msgText;
+		if (!calculateSMSTextLength(entryText, smsCharCount, smsMaxCharCount)) {
+			return false;
+		}
+
+		if (smsCharCount > smsMaxCharCount) {
+			DBG("Input limit reached!");
+			//TODO Add notification about limit here!
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	void RejectMsgEditorController::onBackKeyPressed()
+	{
+		makeDestroyReqeuest();
+	}
+
+	bool RejectMsgEditorController::calculateSMSTextLength(const std::string &smsText, int &charCount, int &maxCharCount)
+	{
+		const int maxGsm7Len = 160;
+		const int maxUnicodeLen = 70;
+
+		msg_encode_type_t encode = MSG_ENCODE_GSM7BIT;
+		unsigned textLen = 0;
+		unsigned segmentLen = 0;
+		int bytesInChar = 1;
+
+		if (!smsText.empty()) {
+			int resultCode = msg_util_calculate_text_length(smsText.c_str(), MSG_ENCODE_AUTO, &textLen, &segmentLen, &encode);
+			if (resultCode != MSG_SUCCESS) {
+				ERR("msg_calculate_text_length is failed ret = %d", resultCode);
+				return false;
+			}
+
+			switch(encode) {
+			case MSG_ENCODE_UCS2:
+				bytesInChar = 2;
+				break;
+
+			case MSG_ENCODE_GSM7BIT:
+			case MSG_ENCODE_AUTO:
+				bytesInChar = 1;
+				break;
+
+			default:
+				ERR("Unknown encode type: ", encode);
+				break;
+			}
+
+			textLen /= bytesInChar;
+			segmentLen /= bytesInChar;
+		}
+
+		if (segmentLen == 0) {
+			segmentLen = encode == MSG_ENCODE_UCS2 ? maxUnicodeLen : maxGsm7Len;
+		}
+
+		charCount = textLen;
+		maxCharCount = segmentLen;
+
+		return true;
+	}
+
+
+} }
