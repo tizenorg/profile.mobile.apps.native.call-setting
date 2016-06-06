5cb3c112f156c83605fd6fa721d60ee2cc069241
Andriy Kolesnichenko <a.kolesniche@samsung.com>
2016-06-06 15:40:43 +0300

    [Call Settings] Set ony 0 degrees orienatation support according to Contacts concept
    Change-Id: Iadbb5c514291e8ec579174ac2d27739aacdb26b1
    Signed-off-by: Andriy Kolesnichenko <a.kolesniche@samsung.com>

--- common/src/gui/ViewManager.cpp
+++ common/src/gui/ViewManager.cpp
@@ -74,17 +74,11 @@ namespace gui {
 		elm_win_conformant_set(m_pWindow, EINA_TRUE);
 
 		if (elm_win_wm_rotation_supported_get(m_pWindow)) {
-			int rotationAngels[4] = { 0, 90, 180, 270 };
-			elm_win_wm_rotation_available_rotations_set(m_pWindow, rotationAngels, 4);
+			int rotationAngels[1] = { 0 };
+			elm_win_wm_rotation_available_rotations_set(m_pWindow, rotationAngels, 1);
 			evas_object_smart_callback_add(m_pWindow, "wm,rotation,changed",
 				EoSmartCb::make<ViewManager, &ViewManager::onOrientationChanged>(), this);
 		}
-
-		Evas_Object *bg = elm_bg_add(m_pWindow);
-		RETVM_IF(!bg, false, "Failed to create bg!");
-		evas_object_size_hint_weight_set(bg, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-		elm_win_resize_object_add(m_pWindow, bg);
-		evas_object_show(bg);
 
 		m_pConformant = elm_conformant_add(m_pWindow);
 		RETVM_IF(!m_pConformant, false, "Failed to create conformant!");
